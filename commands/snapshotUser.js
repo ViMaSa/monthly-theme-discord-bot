5c53bba985389a99a72b42bfe8b1bd882b7629cb
Victor Sarmiento <sarmiv07@gmail.com>
2023-02-07 20:39:58 -0800

    updated snapshot command to account for role & channel the command is typed in

--- commands/snapshotUser.js
+++ commands/snapshotUser.js
@@ -1,6 +1,47 @@
 const { SlashCommandBuilder } = require('discord.js');
 
-// TODO: snapshot not complete, just tesing how to get member information for later
+const checkChannel = (interaction) => {
+  return interaction.channel.name === 'monthly-theme-general';
+};
+
+const checkUserRole = (interaction) => {
+  const role = interaction
+    .guild
+    .roles
+    .cache
+    .find(cachedRole => cachedRole.name === 'Monthly Theme - Participant');
+
+  return role.members.some(member => member.user.id === interaction.user.id);
+};
+
+const snapshotReplies = async (interaction, user) => {
+  const userHasRole = checkUserRole(interaction);
+  const correctChannel = checkChannel(interaction);
+  const replyObj = {
+    content: '',
+    ephemeral: false,
+  };
+
+  if (userHasRole && correctChannel) {
+    replyObj.content = `${user.displayName} \n ${user.displayAvatarURL()}`;
+  }
+  else {
+    replyObj.ephemeral = true;
+
+    if (!userHasRole) {
+      replyObj.content =
+        `${user.displayName} does not have the proper role assigned for this command! `;
+    }
+
+    if (!correctChannel) {
+      replyObj.content +=
+        'This command can only be used in the __**monthly-theme-general**__ text chat under the __**Monthly-Theme-Category**__!';
+    }
+  }
+
+  await interaction.reply(replyObj);
+};
+
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('snapshot')
@@ -10,9 +51,9 @@ module.exports = {
         .setName('member')
         .setDescription('The member to get information')),
 
-  async execute(interaction) {
+  execute(interaction) {
     const user = interaction.options.getMember('member');
 
-    await interaction.reply(`${user.displayName} \n ${user.displayAvatarURL()} `);
+    snapshotReplies(interaction, user);
   },
 };
